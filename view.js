6c6af0f64c828cc750172053895a416a968fc364
Alberto Cangialosi <billyballo.res@gmail.com>
2023-12-18 00:27:37 +0100

    feat: button with event listeners
    for now it just calls notImplemented()
    
    of course added <script> tags in registri.html
    
    prettier added an indentation  with head and body

--- view.js
+++ view.js
@@ -4,6 +4,41 @@ const notImplemented = () => {
   alert("Not Implemented Yet");
 }
 
+// inserire le manipolazioni del dom qui dentro
+document.addEventListener('DOMContentLoaded', function () {
+  var goToStudentsList = document.querySelector('.ElencoStudenti');
+  var goToLectures = document.querySelector('.BottoneMostraLezioni');
+  var deleteRegister = document.querySelector('.EliminaRegistro');
+
+  // FIXME : Attenzione a questi, le classi sono un bel po' confuse
+  var goToSubject = document.querySelectorAll('.TitoloMateria');
+  // var goToSubject2 = document.querySelector('.TitoloMateria .Chimica'); // NOTE : ne serve uno per ciascun bottone quando creato nel DOM
+
+  // TODO: questo funziona solo sul testo, non sul div arrotondato
+  if (goToStudentsList) {
+    goToStudentsList.addEventListener('click', notImplemented);
+  }
+
+  if (goToLectures) {
+    goToLectures.addEventListener('click', notImplemented);
+  }
+
+  // TODO: questo funziona solo sul testo, non sul div arrotondato
+  if (deleteRegister) {
+    deleteRegister.addEventListener('click', notImplemented);
+  }
+
+  if (goToSubject) {
+    goToSubject.forEach(elem =>
+      elem.addEventListener('click', notImplemented)
+    );
+  }
+
+
+
+
+});
+
 
 // // Export delle funzioni
 // module.exports = {
