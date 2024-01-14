41974e7674ffb28a25c84ce6cdbfd0a07edb5904
Gabriele Di Grazia <gabrii539@gmail.com>
2024-01-14 12:25:35 +0100

    fix: button to add register now clones itself

--- view.js
+++ view.js
@@ -1,20 +1,20 @@
 // File per modificare il Prensentation Layer (aka aggire sull' HTML modificando il DOM)
 
 //DOM declarations
-const btn = document.querySelectorAll('.Materia')[1];
-const materia = document.querySelectorAll('.Materia')[0];
+const materie = document.querySelectorAll('.Materia');
+const addMateriaBtn = materie[materie.length - 1];
 const lista = document.querySelector('.Lista');
 
 const notImplemented = () => {
   alert("Not Implemented Yet");
 }
 
-btn.addEventListener('click', () => {
-  const newMateria = materia.cloneNode(true);
+addMateriaBtn.addEventListener('click', () => {
+  const newMateria = addMateriaBtn.cloneNode(true);
 
-  // li > button > h4 > "nuova materia" (innerText)
+  // dobbiamo entrare dentro h4 per mettere il testo
   newMateria.children[0].children[0].innerText = 'Nuova Materia';
-  lista.insertBefore(newMateria, btn);
+  lista.insertBefore(newMateria, addMateriaBtn);
 });
 
 // // Export delle funzioni
