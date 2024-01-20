b54e24befd0712b730ff10d8f31a22d2be4d28d6
Gabriele Di Grazia <49413031+Rizaaal@users.noreply.github.com>
2024-01-20 18:41:04 +0100

    Docs/added declaration of attendee (#45)
    * docs: added declaration of attendee
    
    * chore(info): typos in declaration.js
    
    ~~attendants~~ \*attendees
    ~~argument~~ \*topic
    ~~arrivo~~ \*ingress
    ~~uscita~~ \*exit
    
    * docs(info): update typos under interfaces section
    
    ---------
    
    Co-authored-by: Gabriele Di Grazia <gabrii539@gmail.com>
    Co-authored-by: Alberto Cangialosi <billyballo.res@gmail.com>

--- declarations.js
+++ declarations.js
@@ -19,7 +19,18 @@ let TStudent = {
 // JavaScript equivalent for TAttendance interface
 let TAttendance = {
   id: '',
-  Date:new Date(),
-  argoument: '',
-  students: []//array<{nome, arrivo, uscita}>
+  //"yyyy-MM-ddTh:m:s" This is standardized and will work reliably
+  Date: new Date('yyyy-mm-dd'),
+  topic: '',
+  attendees: [
+    {
+      //id studente? potrebbe bastare e poi semplicemente prendere
+      //nome e cognome dell'id inserito? o sarebbe troppo complicato?
+      // name: '',
+      // lastName: '',
+      studentId: '',
+      ingress: 'hh:mm',
+      exit: 'hh:mm'
+    }
+  ]
 };
